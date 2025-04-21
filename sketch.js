cce4751f4113eb2ddb58ce02ef6430f56ddfff8d
Chenye Wang <chenyewang@Chenyes-MacBook-Pro.local>
2025-04-21 13:12:31 -0400

    Fix audio upload handler scope

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let mic, fft;
 let isMicStarted = false;
 let isAppStarted = false;
+let uploadedSound;
+let isFilePlaying = false;
 
 function setup() {
   let cnv = createCanvas(800, 400);
@@ -9,31 +11,31 @@ function setup() {
 }
 
 function draw() {
-   background(255);
- 
-   if (isAppStarted && (isMicStarted || isFilePlaying)) {
-     let spectrum = fft.analyze();
- 
-     noStroke();
-     fill(0);
-     for (let i = 0; i < spectrum.length; i++) {
-       let x = map(i, 0, spectrum.length, 0, width);
-       let h = -height + map(spectrum[i], 0, 255, height, 0);
-       rect(x, height, width / spectrum.length, h);
-     }
- 
-     let waveform = fft.waveform();
-     noFill();
-     stroke(0);
-     beginShape();
-     for (let i = 0; i < waveform.length; i++) {
-       let x = map(i, 0, waveform.length, 0, width);
-       let y = map(waveform[i], -1, 1, 0, height);
-       vertex(x, y);
-     }
-     endShape();
-   }
- }
+  background(255);
+
+  if (isAppStarted && (isMicStarted || isFilePlaying)) {
+    let spectrum = fft.analyze();
+
+    noStroke();
+    fill(0);
+    for (let i = 0; i < spectrum.length; i++) {
+      let x = map(i, 0, spectrum.length, 0, width);
+      let h = -height + map(spectrum[i], 0, 255, height, 0);
+      rect(x, height, width / spectrum.length, h);
+    }
+
+    let waveform = fft.waveform();
+    noFill();
+    stroke(0);
+    beginShape();
+    for (let i = 0; i < waveform.length; i++) {
+      let x = map(i, 0, waveform.length, 0, width);
+      let y = map(waveform[i], -1, 1, 0, height);
+      vertex(x, y);
+    }
+    endShape();
+  }
+}
 
 function startSketch() {
   isAppStarted = true;
@@ -59,9 +61,7 @@ function startSketch() {
   }).catch((err) => {
     console.error("Tone.js failed to start:", err);
   });
-
-let uploadedSound;
-let isFilePlaying = false;
+}
 
 window.handleUploadedAudio = function(fileURL) {
   if (uploadedSound) {
@@ -79,4 +79,3 @@ window.handleUploadedAudio = function(fileURL) {
     loop();
   });
 };
-}
